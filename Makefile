56746c1dd48eba7fd31bc595dd53017f9f8acb8c
Jasper den Ouden <o.jasper@gmail.com>
2012-10-10 14:01:26 +0200

    Moved util to other project.

--- Makefile
+++ Makefile
@@ -3,11 +3,8 @@ all:
 	echo Currently nothing to be made.\(and don\'t expect anything\); \
 	echo There is a test, however.
 
-test_all: test_util test_glplot
+test: test_glplot
 
-test_util:
-	cd util/test/;\
-	make test
 test_glplot:
 	cd julia-glplot/test/;\
 	make test
--- doc/example_juliarc_part.jl
+++ doc/example_juliarc_part.jl
@@ -1,19 +1,14 @@
 
-#...
+#NOTE: this is out of date, .juliarc.jl doesn't seem to be loaded anymore
+# TODO start using modules...
 
-#_example_ part of juliarc
-begin
-  # Don't want trouble with people thinking that is a julia variable, or it
-  # becoming one in the future and having a bad time.
-  local user="jasper" 
-  local proj="/home/$user/proj"
-#NOTE that used to be /src/julia-src/ (and this is about it's dependency)
-  push(LOAD_PATH, "$proj/common-lisp/parse-c-header/julia-src/")
-  push(LOAD_PATH, "$proj/julia/julia-glplot/")
-end
+#For me is "proj/julia" or "oproj/julia"
+directory_of_julia_projects = "Your directory" 
+push(LOAD_PATH, directory_of_julia_projects)
 
 #....
 
+#And if you want to check that all the directories exist:
 for path in LOAD_PATH #At end.
   size = filesize(path)
   assert( size>0, "Seems like path in LOAD_PATH doesn't exist.
